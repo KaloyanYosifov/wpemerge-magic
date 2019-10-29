02f10742939e4f7d73c819695b8f2694aab90e17
Kaloyan Yosifov <kaloqn665@gmail.com>
2019-10-29 21:29:35 +0200

    Add new layout for the library and integrate magic file

---
+++ app/Bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WPEmergeMagic;
+
+class Bootstrap
+{
+    /**
+     * Handle all bootstrap logic
+     * required for the library to work
+     *
+     * @return void
+     */
+    public function handle() {}
+}
