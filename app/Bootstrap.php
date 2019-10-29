1fc562f9e08866e6f5b28de3e299ccf2965b86ec
Kaloyan Yosifov <kaloqn665@gmail.com>
2019-10-29 21:52:06 +0200

    Integrate first command

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -2,13 +2,55 @@
 
 namespace WPEmergeMagic;
 
+use WPEmergeMagic\Console\Console;
+
 class Bootstrap
 {
+    /**
+     * Console class
+     *
+     * @var [WPEmergeMagic\Console\Console]
+     */
+    protected $console = null;
+
+    public function __construct()
+    {
+        $this->console = new Console;
+    }
+
     /**
      * Handle all bootstrap logic
      * required for the library to work
      *
      * @return void
      */
-    public function handle() {}
+    public function handle()
+    {
+        define('ROOT_DIR', __DIR__ . '..' . DIRECTORY_SEPARATOR);
+        define('APP_DIR', __DIR__);
+
+        $this->register();
+        $this->run();
+    }
+
+    /**
+     * All configuration required to be done
+     * before executing
+     *
+     * @return void
+     */
+    protected function register()
+    {
+        $this->console->register();
+    }
+
+    /**
+     * Executing code
+     *
+     * @return void
+     */
+    protected function run()
+    {
+        $this->console->run();
+    }
 }
--- app/Console/Console.php
+++ app/Console/Console.php
@@ -2,6 +2,7 @@
 
 namespace WPEmergeMagic\Console;
 
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\Console\Application;
 
 class Console
@@ -18,10 +19,33 @@ class Console
         $this->application = new Application();
     }
 
-    public function register() {}
+    public function register()
+    {
+        $this->registerAllCommands();
+    }
 
     public function run()
     {
         $this->application->run();
+    }
+
+    protected function registerAllCommands()
+    {
+        $COMMANDS_PATH = APP_DIR . DIRECTORY_SEPARATOR . 'Console' . DIRECTORY_SEPARATOR . 'Commands';
+
+        $commandsFinder = new Finder;
+        $commandsFinder->files()->in($COMMANDS_PATH);
+
+        if (!$commandsFinder->hasResults()) {
+            throw new \RuntimeException('Couldn\'t load commands!');
+        }
+
+        foreach ($commandsFinder as $commandFile) {
+            $commandClassName = explode('.', $commandFile->getRelativePathName())[0];
+            $commandNamespaceClass = 'WPEmergeMagic\\Console\\Commands\\';
+            $commandClass = $commandNamespaceClass . $commandClassName;
+
+            $this->application->add(new $commandClass);
+        }
     }
 }
