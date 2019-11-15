6df251a5cc53bed15b4f44a10f5d591570c7d58c
Kaloyan Yosifov <kaloqn665@gmail.com>
2019-11-15 19:48:46 +0200

    Fix naming convention for controller is created

--- tests/Unit/Console/Commands/MakeControllerCommandTest.php
+++ tests/Unit/Console/Commands/MakeControllerCommandTest.php
@@ -9,12 +9,12 @@ class MakeControllerCommandTest extends CommandTestCase
     /** @test */
     public function it_can_create_three_types_of_controllers()
     {
-        $this->testCreatingAController('TestController');
-        $this->testCreatingAController('TestController', 'admin');
-        $this->testCreatingAController('TestController', 'ajax');
+        $this->assertControllerIsCreated('TestController');
+        $this->assertControllerIsCreated('TestController', 'admin');
+        $this->assertControllerIsCreated('TestController', 'ajax');
     }
 
-    protected function testCreatingAController(string $name, string $type = 'web')
+    protected function assertControllerIsCreated(string $name, string $type = 'web')
     {
         $this->runCommand('make:controller', [
             'name' => $name,
