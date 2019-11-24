ff87d1904c50aa98f946a6d22d7c321bf65215d1
Kaloyan Yosifov <kaloqn665@gmail.com>
2019-11-24 11:40:37 +0200

    Support parsing functions in js as a string for json

--- app/Parsers/JsParser.php
+++ app/Parsers/JsParser.php
@@ -7,14 +7,82 @@ class JsParser
     public function parse(string $jsObject)
     {
         $lines = explode(PHP_EOL, $jsObject);
+        $lineIndex = 0;
+        $lineCount = count($lines);
         $newLines = '';
+        $addTrailingComma = true;
 
-        foreach ($lines as $line) {
+        for ($lineIndex = 0; $lineIndex < $lineCount; $lineIndex++) {
+
+            // if the next line is the last line
+            // we try to figure out line count by adding 2, so we compensate for starting from 0
+            // we start from 0 and get the first line
+            // but the count starts from 1
+            // so if we want to get the next line we have to add 1 surplus due to this difference
+            $line = $lines[$lineIndex];
+            $isNextLineEnd = $lineIndex + 2 === $lineCount;
+            $atEndLine = $lineIndex + 2 > $lineCount;
+            $nextLine = !$atEndLine ? $lines[$lineIndex + 1] : '';
+
+            if (preg_match('~{$~', $line)) {
+                $addTrailingComma = true;
+            }
+
+            // if after all the space
+            // we start with a closed curly bracket
+            // do not add a triling comma
+            if (preg_match('~^\s+}~', $nextLine)) {
+                $addTrailingComma = false;
+            }
+
+            if (strpos($line, '(') !== false) {
+                $newLines .= $this->parseFunctionLineInJsObject($line, $addTrailingComma);
+                continue;
+            }
+
             $line = preg_replace('~\'~', '"', $line);
-            $line = preg_replace('~(?<!:)(?<!,)(\s+)(.*?):~', '$1"$2":', $line);
-            $newLines .= $line . PHP_EOL;
+
+            // if the next line is the last line
+            // check if we have a trailing comma
+            // and if we do remove it
+            if ($isNextLineEnd) {
+                $line = preg_replace('~,$~', '', $line);
+            }
+
+            $newLines .= $this->addDoubleQuotesToObjects($line) . PHP_EOL;
         }
 
         return json_decode($newLines, true);
+    }
+
+    protected function parseFunctionLineInJsObject(string $line, bool $addTrailingComma): string
+    {
+        $splittedLineByObject = explode(':', $line);
+
+        // we are poping the object from the spllited line objects
+        // so we can wrap it in double quotes
+        // and we are not going to touch the right half
+        $objectName = $this->addDoubleQuotesToObjects(
+            array_shift($splittedLineByObject) . ':'
+        );
+
+        // glue the right side and remove the space on the start
+        $gluedRightSide = preg_replace('~^\s|,$~', '', implode(':', $splittedLineByObject));
+
+        // add double qoutes to the enire right thalf of the splitted line objects
+        $parsedLine = $objectName . ' "' . $gluedRightSide . '"';
+
+        if ($addTrailingComma) {
+            $parsedLine .= ',';
+        }
+
+        return $parsedLine . PHP_EOL;
+    }
+
+    protected function addDoubleQuotesToObjects(string $line): string
+    {
+        $line = preg_replace('~(?<!:)(?<!,)(\s+)(.*?):~', '$1"$2":', $line);
+
+        return $line;
     }
 }
--- tests/Unit/Parsers/JsParserTest.php
+++ tests/Unit/Parsers/JsParserTest.php
@@ -44,4 +44,29 @@ class JsParserTest extends TestCase
             }
         }
     }
+
+    /** @test */
+    public function it_parses_js_object_with_functions()
+    {
+        $jsObject = <<<'EOD'
+        {
+            object1: {
+                welcome: classObject.parsingText('testing'),
+                complex: classObject.parsingText('testing', 'test123', 'testing the one two three'),
+                complex2: classObject.parsingText('testing', ['array2', 'arra3', ' asd' ]),
+                complex3: classObject.parsingText('testing', __dirname('testing_sunshine')),
+            },
+        }
+        EOD;
+
+        $data = (new JsParser())->parse($jsObject);
+
+        $this->assertArrayHasKey('object1', $data);
+        $this->assertArrayHasKey('welcome', $data['object1']);
+
+        $this->assertSame("classObject.parsingText('testing')", $data['object1']['welcome']);
+        $this->assertSame("classObject.parsingText('testing', 'test123', 'testing the one two three')", $data['object1']['complex']);
+        $this->assertSame("classObject.parsingText('testing', ['array2', 'arra3', ' asd' ])", $data['object1']['complex2']);
+        $this->assertSame("classObject.parsingText('testing', __dirname('testing_sunshine'))", $data['object1']['complex3']);
+    }
 }
